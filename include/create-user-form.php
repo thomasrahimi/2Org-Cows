1d3efff29a311f45b13c377d1a2d862a58207965
hollyclergyman <thomas.rahimi@openmailbox.org>
2017-04-11 10:46:31 +0200

    username help added

--- include/create-user-form.php
+++ include/create-user-form.php
@@ -1,7 +1,12 @@
 <form action="./scripts/create_user_script.php" method="POST">
 				<table>
 					<tr><td>Full name of user *</td><td><input type="text" name="fullname" required /></td></tr>
-					<tr><td>Username *</td><td><input type="text" name="username" required placeholder="name in small letters"/></td></tr>
+					
+					<tr><td>Username *</td>
+						<td><div class="tooltip"><input type="text" name="username" required /><span class="tooltiptext">Please choose a small letter username</span>
+							</div>
+						</td>
+					</tr>
 					<tr><td>Password *</td><td><input type="password" name="password" required /></tr>
 					<tr><td>Repeat Password *</td><td><input type="password" name="repeat_password" required /></td>
 					<td>
