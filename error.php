3c89a4c752aff8f4ad160f91a2ec500d1bb90c89
hollyclergyman <thomas.rahimi@openmailbox.org>
2017-05-26 20:15:50 +0200

    Corrected error in paths

--- error.php
+++ error.php
@@ -9,7 +9,7 @@ $user_role = $_SESSION["role"];
 <html>
 <head>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" type="text/css" href="./static/2Org-Cows.css" />
+	<link rel="stylesheet" type="text/css" href="../static/2Org-Cows.css" />
 	<link rel="shortcut icon" href="./static/Favicon-2Org-Cows.ico" type="image"/>
 	<title>Content not found</title>
 </head>
@@ -39,11 +39,11 @@ $user_role = $_SESSION["role"];
 		</div>
 	</div>
 	<div class="logo">
-	<a href="./search.php">
+	<a href="../search.php">
 		<picture>
-		<source srcset="./static/Logo-2Org-Cows.png" media="(min-width: 800px)">
-		<source srcset="./static/Logo-2Org-Cows64.png">
-		<img src="./static/Logo-2Org-Cows.png" alt="2Org-Cows logo">
+		<source srcset="../static/Logo-2Org-Cows.png" media="(min-width: 800px)">
+		<source srcset="../static/Logo-2Org-Cows64.png">
+		<img src="../static/Logo-2Org-Cows.png" alt="2Org-Cows logo">
 		</picture>
 	</a>
 	</div>
